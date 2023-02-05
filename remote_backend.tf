9db3c13355f3bff482e316bbef1f09b8e0f1873e
Fabio Chagas <fabio.schagas@outlook.com>
2023-02-05 18:20:41 +0000

    Added the second tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -8,14 +8,4 @@ terraform {
     }
   }
 }
-=======
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "lab-code"
-    workspaces {
-      name = "hashicat-aws"
-    }
-  }
-}
->>>>>>> e6d2a0b858204ec32ae9576e25a3cb6dd21d74cc
+
