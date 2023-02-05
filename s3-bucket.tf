6a6c9e881b3c4e369f8db726b58efe118d4c7479
Fabio Chagas <fabio.schagas@outlook.com>
2023-02-05 19:03:44 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,11 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-0001"
+  bucket = "my-s3-fabiochagas-0001"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-  
-  bucket_prefix = "fabio-chagas"
 
 }
